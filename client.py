76668ab4ca962597234f05ae6824ad4ea59375bb
Varsto04 <ilya-ovchinnikov-04-04@yande.ru>
2022-12-18 13:40:07 +0300

    Update

--- client.py
+++ client.py
@@ -10,6 +10,7 @@ import rocket as rocket
 import server as server
 import explosion as expl
 import icons as icons
+import arcade.gui
 
 
 BUFSIZE = 1024
@@ -63,6 +64,172 @@ class SpaceGameClient:
         game = ClientGame()
         window.show_view(game)
         arcade.run()
+
+
+class StartView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.background = None
+
+        self.texture = arcade.load_texture(":resources:images/backgrounds/stars.png")
+
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
+
+        self.v_box = arcade.gui.UIBoxLayout()
+
+        sound_button = arcade.Sound(file_name='Sounds/Retro3.wav')
+
+        button_style = {
+            "font_size": 30,
+            "font_color": arcade.color.AQUA,
+            "border_width": 2,
+            "border_color": arcade.color.AQUA,
+            "bg_color": None,
+
+            "bg_color_pressed": None,
+            "border_color_pressed": arcade.color.BLUE,
+            "font_color_pressed": arcade.color.BLUE,
+        }
+
+        start_button = arcade.gui.UIFlatButton(text="Начать", width=400, height=100, font_size=100, style=button_style)
+        self.v_box.add(start_button.with_space_around(bottom=20))
+
+        authors_button = arcade.gui.UIFlatButton(text="Авторы", width=400, height=100, style=button_style)
+        self.v_box.add(authors_button.with_space_around(bottom=20))
+
+        quit_button = arcade.gui.UIFlatButton(text="Выход", width=400, height=100, style=button_style)
+        self.v_box.add(quit_button.with_space_around(bottom=20))
+
+        @start_button.event("on_click")
+        def on_click_start(event):
+            arcade.play_sound(sound_button)
+            game_view = EnterView()
+            self.window.show_view(game_view)
+            self.manager.disable()
+
+        @quit_button.event("on_click")
+        def on_click_quit(event):
+            arcade.play_sound(sound_button)
+            arcade.exit()
+
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="center_x",
+                anchor_y="center_y",
+                child=self.v_box,)
+        )
+
+        arcade.set_background_color((0, 0, 0))
+
+    def on_draw(self):
+        self.clear()
+        self.texture.draw_sized(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2,
+                                WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.manager.draw()
+
+
+class EnterView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.background = None
+
+        self.texture = arcade.load_texture(":resources:images/backgrounds/stars.png")
+
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
+
+        self.v_box = arcade.gui.UIBoxLayout()
+
+        self.sound_button = arcade.Sound(file_name='Sounds/Retro3.wav')
+
+        button_style = {
+            "font_size": 30,
+            "font_color": arcade.color.AQUA,
+            "border_width": 2,
+            "border_color": arcade.color.AQUA,
+            "bg_color": None,
+
+            "bg_color_pressed": None,
+            "border_color_pressed": arcade.color.BLUE,
+            "font_color_pressed": arcade.color.BLUE,
+        }
+
+        self.input_field = arcade.gui.UIInputText(text="Введите сюда ваш ник", width=650, height=40, font_size=24,
+                                                  text_color=arcade.color.AQUA)
+        self.v_box.add(self.input_field)
+
+        self.label = arcade.gui.UILabel(text=f"#Ваш ник: ", width=650, height=40, font_size=16,
+                                        text_color=arcade.color.AQUA)
+        self.v_box.add(self.label.with_space_around(bottom=0))
+
+        self.label2 = arcade.gui.UILabel(text="", width=650, height=40, font_size=16,
+                                         text_color=arcade.color.AQUA)
+
+        submit_button = arcade.gui.UIFlatButton(text="Обновить", width=400, height=40, style=button_style)
+        self.v_box.add(submit_button.with_space_around(bottom=10))
+        submit_button.on_click = self.on_click
+
+        clear_button = arcade.gui.UIFlatButton(text="Очистить", width=400, height=40, style=button_style)
+        self.v_box.add(clear_button.with_space_around(bottom=300))
+
+        start_button = arcade.gui.UIFlatButton(text="Продолжить", width=400, height=100, style=button_style)
+        self.v_box.add(start_button.with_space_around(bottom=20))
+
+        back_button = arcade.gui.UIFlatButton(text="Назад", width=400, height=100, style=button_style)
+        self.v_box.add(back_button)
+
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="center_x",
+                anchor_y="center_y",
+                child=self.v_box, )
+        )
+
+        @clear_button.event("on_click")
+        def on_click_start(event):
+            arcade.play_sound(self.sound_button)
+            self.input_field.text = ''
+
+        @start_button.event("on_click")
+        def on_click_start(event):
+            arcade.play_sound(self.sound_button)
+            if (len(self.input_field.text) <= 20) and (self.input_field.text != 'Введите сюда ваш ник'):
+                game_view = Game()
+                game_view.setup()
+                self.window.show_view(game_view)
+                self.manager.disable()
+            else:
+                if len(self.input_field.text) > 20:
+                    self.label2.text = '#Ваш ник не должен превышать 20 символов'
+                    self.v_box.add(self.label2.with_space_around(bottom=0))
+                elif self.input_field.text == 'Введите сюда ваш ник':
+                    self.label2.text = '#Вы забыли изменить ник или нажать кнопку "Обновить"'
+                    self.v_box.add(self.label2.with_space_around(bottom=0))
+
+        @back_button.event("on_click")
+        def on_click_quit(event):
+            arcade.play_sound(self.sound_button)
+            game_view = StartView()
+            self.window.show_view(game_view)
+            self.manager.disable()
+
+        arcade.set_background_color((0, 0, 0))
+
+    def on_draw(self):
+        self.clear()
+        self.texture.draw_sized(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2,
+                                WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.manager.draw()
+
+    def update_text(self):
+        self.label.text = f'#Ваш ник: {self.input_field.text}'
+        global nickname
+        nickname = self.input_field.text
+
+    def on_click(self, event):
+        arcade.play_sound(self.sound_button)
+        self.update_text()
 
 
 class ClientGame(arcade.View):
@@ -482,7 +649,7 @@ class TCPSend(Thread):
     def run(self):
         while True:
             self.send_data()
-            time.sleep(0.00005)
+            time.sleep(0.000005)
 
     def send_data(self):
         # отправление на сервер адреса, координат и угол клиента
@@ -740,7 +907,7 @@ class TCPReciv(Thread):
                                 bullet_list_mutex.acquire()
                                 bullet_list.append(self.bullet_sprite_2)
                                 bullet_list_mutex.release()
-                            #arcade.play_sound(self.sound_laser, volume=1)
+
                         if shot_type[1] == '1':
                             for i in range(0, len(sprite_players_list)):
                                 if int(sprite_players_list[i].address.split(':')[1]) == int(cur_data[0]):
